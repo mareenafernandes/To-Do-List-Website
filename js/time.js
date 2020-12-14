49c49f11c2234fcd46a7c1207dd9253bd5fdd017
Mareena Fernandes <56290461+mareenafernandes@users.noreply.github.com>
2020-12-14 10:41:31 +0530

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,2 +1,10 @@
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
+function display_c(){
+var refresh=1000;
+mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+var x = new Date();
+document.getElementById('ct').innerHTML = x;
+display_c();
+}
